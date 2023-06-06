e95c003b0d117adfd5c70964740f942a82aaea97
EliasHaynes <eliasmhaynes@gmail.com>
2023-06-06 16:54:54 -0500

    Protected Routes with Auth0

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,6 +1,7 @@
 // Routing Tools
-import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter,Routes, Route, Navigate} from "react-router-dom";
 import cookie from 'cookie'
+import { useAuth0 } from "@auth0/auth0-react";
 
 // Imported Components
 import Home from "./components/Home";
@@ -16,8 +17,30 @@ import ScheduledMaintenance from "./components/ScheduledMaintenance";
 
 
 
-//Frontend Login Auth
+//Protecting Routes w/ Auth0
+  const ProtectedRoute =(props) => {
+    const {isAuthenticated} = useAuth0();
+    const {component: Component, ...rest} =props;
 
+    return(
+        isAuthenticated === true ? (<Component {...rest} />): (<Navigate to='/' />)
+    )
+}
+
+// const checkCarRegistered = () => {
+//     const cookies = cookie.parse(document.cookie);
+//     console.log(cookies)
+//     return cookies["loggedIn"] ? true : false;
+// }
+
+// const carInfoSubmitted= (props) => {
+    
+//     const {component: Component, ...rest} =props;
+
+//     return(
+//         checkCarRegistered() === true ? (<Component {...rest} />): (<Navigate to='/' />)
+//     )
+// }
 
 
 
@@ -27,21 +50,20 @@ function Router() {
         return (
             <Routes>
             {/* Home */}
-            <Route path="/" element={<Home/>} />
+            <Route path="/" element={<Home />} />
     
             {/* API Components */}
-            <Route path="/vindecode" element={VinDecode} />
-            <Route path="/userMaintenace" element={ScheduledMaintenance} />
+            <Route path="/vindecode" element={<ProtectedRoute component={VinDecode} />} />
+            <Route path="/userMaintenace" element={<ProtectedRoute component={ScheduledMaintenance} />} />
     
             {/* Repair Components */}
-            <Route path="/repair" element={<RepairLog/>} />
-            <Route path='/create' element={<AddRepair />} />
-            <Route path="/update/:id" element={<UpdateRepair/>} />
+            <Route path="/repair" element={<ProtectedRoute component={RepairLog} />} />
+            <Route path='/create' element={<ProtectedRoute component={AddRepair} />} />
+            <Route path="/update/:id" element={<ProtectedRoute component={UpdateRepair} />} />
     
             {/* Auth Components */}
-            <Route path="/login" element={<Login/>} />
-            <Route path="/registerCar" element={RegisterCar} />
-            <Route path="/registerUser" element={<RegisterUser/>} />
+            <Route path="/login" element={<ProtectedRoute component={Login}/>} />
+            <Route path="/registerCar" element={<ProtectedRoute component={RegisterCar} />} />
             
         </Routes>
         )
