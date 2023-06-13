aaff02e6cc28d32558dc4991a66b85898b656366
EliasHaynes <eliasmhaynes@gmail.com>
2023-06-13 18:35:06 -0500

    showing repairs by auht0_id made

---
+++ backend/controllers/auth.js
@@ -0,0 +1,14 @@
+[
+    {
+        "id": 10,
+        "date": null,
+        "mileage": 30,
+        "maintenance": "boo",
+        "performedBy": "boo",
+        "contact": "boo",
+        "material": 30,
+        "labor": 30,
+        "other": 30,
+        "total": null
+    }
+]
