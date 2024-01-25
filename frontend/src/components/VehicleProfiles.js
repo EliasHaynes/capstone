e45252fefc861771055dbecc99b764cdb813191b
EliasHaynes <eliasmhaynes@gmail.com>
2024-01-24 20:34:16 -0600

    Minor styling VehicleProfile. Major Database restructure.

--- frontend/src/components/VehicleProfiles.js
+++ frontend/src/components/VehicleProfiles.js
@@ -2,66 +2,72 @@ import React, { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
 function VehicleProfiles() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [selectedProfile, setProfile] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("The selected profile is...", selectedProfile)
+    console.log("The selected profile is...", selectedProfile);
   };
 
   const handleProfileSelection = (e) => {
     setProfile(e.target.value);
     console.log(e.target.value);
   };
-  console.log("Current Profile:", selectedProfile);
+
   return (
-    <div>
-      <h1>Vehicle Profiles</h1>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <label className="container">
-          One
-          <input
-            onClick={(e) => handleProfileSelection(e)}
-            type="radio"
-            name="radio"
-            value="one"
-          />
-          <span className="checkmark"></span>
-        </label>
-        <label className="container">
-          Two
-          <input
-            onClick={(e) => handleProfileSelection(e)}
-            type="radio"
-            name="radio"
-            value="two"
-          />
-          <span className="checkmark"></span>
-        </label>
-        <label className="container">
-          Three
-          <input
-            onClick={(e) => handleProfileSelection(e)}
-            type="radio"
-            name="radio"
-            value="three"
-          />
-          <span className="checkmark"></span>
-        </label>
-        <label className="container">
-          Four
-          <input
-            onClick={(e) => handleProfileSelection(e)}
-            type="radio"
-            name="radio"
-            value="four"
-          />
-          <span className="checkmark"></span>
-        </label>
-        <button type="submit">Select Vehicle Profile</button>
-      </form>
-      <button onClick={()=> navigate("/vindecode")}>Add Another Vehicle Profile</button>
+    <div className="vehicle-profile-wrap-container">
+      <div className="vehicle-profile-container">
+        <h1>Vehicle Profiles</h1>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label className="container">
+            <input
+              onClick={(e) => handleProfileSelection(e)}
+              type="radio"
+              name="radio"
+              value="one"
+            />{" "}
+            One
+            <span className="checkmark"></span>
+          </label>
+          <label className="container">
+            <input
+              onClick={(e) => handleProfileSelection(e)}
+              type="radio"
+              name="radio"
+              value="two"
+            />{" "}
+            Two
+            <span className="checkmark"></span>
+          </label>
+          <label className="container">
+            <input
+              onClick={(e) => handleProfileSelection(e)}
+              type="radio"
+              name="radio"
+              value="three"
+            />{" "}
+            Three
+            <span className="checkmark"></span>
+          </label>
+          <label className="container">
+            <input
+              onClick={(e) => handleProfileSelection(e)}
+              type="radio"
+              name="radio"
+              value="four"
+            />{" "}
+            Four
+            <span className="checkmark"></span>
+          </label>
+          <div>
+          <button type="submit">Select Vehicle Profile</button>
+          </div>
+        </form>
+        <button onClick={() => navigate("/registerCar")}>
+          Add Another Vehicle Profile
+        </button>
+      </div>
     </div>
   );
 }
