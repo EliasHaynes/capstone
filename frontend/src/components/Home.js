b04f059fe8cccdaba6aa3d2e3aba21241371a886
EliasHaynes <eliasmhaynes@gmail.com>
2024-02-17 07:48:41 -0600

    Improved Home display based on authentication

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -21,10 +21,109 @@ function Home() {
   const navigate = useNavigate();
 
   const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
-  console.log("The user:", user)
+  console.log("The user:", user);
   return (
-    <div className="home">
+    <div className="desktop">
       {!isAuthenticated ? (
+        <div className="landing-main-display">
+          <button
+            className="login-button"
+            onClick={() => {
+              loginWithRedirect();
+              console.log("user from button", user);
+            }}
+          >
+            Login / Register
+          </button>
+        </div>
+      ) : (
+        <Dashboard></Dashboard>
+      )}
+
+      <h1 className="services">Services</h1>
+      <div className="wrap-page-buttons">
+        <div className="page-buttons">
+          <section>
+            <div className="page-buttons-img-container-left first-pic"></div>
+            <div className="page-buttons-text-box">
+              <h3>Register your vehicles profile</h3>
+              <p>
+                Create your vehicles profile to have easy access to your
+                vehicles data.
+              </p>
+              <h4 className="page-buttons-anchors">
+                <a
+                  onClick={() =>
+                    !isAuthenticated
+                      ? loginWithRedirect()
+                      : navigate("/registerCar")
+                  }
+                >
+                  Click Here to register
+                </a>
+              </h4>
+            </div>
+          </section>
+
+          <section className="landing-alternate-background">
+            <div className="page-buttons-text-box">
+              <h3>Get Specs on your Vehicle</h3>
+              <p> </p>
+              <h4 className="page-buttons-anchors">
+                <a
+                  onClick={() =>
+                    !isAuthenticated
+                      ? loginWithRedirect()
+                      : navigate("/vindecode")
+                  }
+                >
+                  Click here to view your vehicles dashboard
+                </a>
+              </h4>
+            </div>
+            <div className="page-buttons-img-container-right second-pic"></div>
+          </section>
+
+          <section>
+            <div className="page-buttons-img-container-left third-pic"></div>
+
+            <div className="page-buttons-text-box">
+              <h3>Get Relative Scheduled Maintenance</h3>
+              <p> </p>
+              <h4 className="page-buttons-anchors">
+                <a
+                  onClick={() =>
+                    !isAuthenticated
+                      ? loginWithRedirect()
+                      : navigate("/userMaintenace")
+                  }
+                >
+                  {" "}
+                  Click here to view upcoming maintenance
+                </a>
+              </h4>
+            </div>
+          </section>
+
+          <section className="landing-alternate-background">
+            <div className="page-buttons-text-box">
+              <h3>Document personal and professional repairs</h3>
+              <p> </p>
+              <h4 className="page-buttons-anchors">
+                <a
+                  onClick={() =>
+                    !isAuthenticated ? loginWithRedirect() : navigate("/repair")
+                  }
+                >
+                  Click here to view repair records
+                </a>
+              </h4>
+            </div>
+            <div className="page-buttons-img-container-right fourth-pic"></div>
+          </section>
+        </div>
+      </div>
+      {/* {!isAuthenticated ? (
         <div>
         <LandingPage></LandingPage>
         <MobileLandingPage></MobileLandingPage>
@@ -34,7 +133,7 @@ function Home() {
           <LandingPage></LandingPage>
           <MobileLandingPage></MobileLandingPage>
           </div>
-      )}
+      )} */}
     </div>
   );
 }
