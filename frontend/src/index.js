fbc94c107f0e20e46697a58543c385f0ed94829c
EliasHaynes <eliasmhaynes@gmail.com>
2024-05-25 12:15:43 -0500

    Added Redux Toolkit. Made maintenance data persistent. Maintenance data clears on vehicle profile switch as well as the alerts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
+import {Provider} from 'react-redux'
+import store from './redux/store'
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -20,7 +22,9 @@ root.render(
       useRefreshTokens={true}
 
     >
+      <Provider store={store}>
       <App />
+      </Provider>
     </Auth0Provider>
   </React.StrictMode>
 );
