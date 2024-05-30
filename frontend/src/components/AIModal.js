12521954402294a3b42a8770e83356f199db0f80
EliasHaynes <eliasmhaynes@gmail.com>
2024-05-29 22:17:54 -0500

    AI content displays on modal. Added loading condition while waiting on AI response

--- frontend/src/components/AIModal.js
+++ frontend/src/components/AIModal.js
@@ -3,11 +3,12 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import axios from 'axios';
+import store from '../redux/store';
 
 
 function AIModal({args, cardID, cardDesc}) {
     const [modal, setModal] = useState(false);
-    const [loading,setLoading] = useState()
+    const [loading,setLoading] = useState(true)
     const [aiResponse, storeAIResponse] = useState("")
     const [cardDescription, setCardDesc] = useState("")
   console.log("cardDesc:", cardDescription)
@@ -24,6 +25,8 @@ function AIModal({args, cardID, cardDesc}) {
       try {
         const response = await axios.get('http://localhost:5000/aiModal', {cardDescription});
         console.log("response:", response.data)
+        storeAIResponse(response.data)
+        setLoading(false)
       } catch (e) {
         console.error(e);
       }
@@ -33,15 +36,9 @@ function AIModal({args, cardID, cardDesc}) {
       <div>
         <AutoAwesomeIcon className='ai-icon' onClick={() => {toggle(); getAIResponse();}}></AutoAwesomeIcon>
         <Modal isOpen={modal} toggle={toggle} {...args}>
-          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+          <ModalHeader toggle={toggle}>{cardDescription}</ModalHeader>
           <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            {loading ? "Loading AI Response..." : aiResponse}
           </ModalBody>
           <ModalFooter>
             <Button color="primary" onClick={toggle}>
